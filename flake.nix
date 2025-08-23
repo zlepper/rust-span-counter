0f0788605500d619718047d322232a8710f97dd4
zlepper <hansen13579@gmail.com>
2025-08-23 19:49:41 +0200

    Add nix flake for easier installation

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "CLI tool that extracts strings from Rust files and provides word-by-word character spans";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "rust-span-counter";
+            version = "0.1.0";
+
+            src = ./.;
+
+            cargoHash = "sha256-m2yCZsFAUjM+cSJDzM5IgL3a/ySOeuF1cPpIt/rW9Hg=";
+
+            meta = with pkgs.lib; {
+              description = "CLI tool that extracts strings from Rust files and provides word-by-word character spans";
+              homepage = "https://github.com/user/rust-span-counter";
+              license = licenses.mit;
+              maintainers = [ ];
+            };
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/rust-span-counter";
+        };
+      });
+    };
+}
